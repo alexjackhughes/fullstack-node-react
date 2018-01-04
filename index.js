aef85b69f9700dad6f781831f1078f7d5bb1b13b
Hughes <alex@alexjackhughes.com>
2018-01-04 16:40:27 +0000

    stage three complete

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express(); // creates the express application.
 
-// The first express route handler
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+/*
+  Authentication Implementation:
+ */
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+  }
+));
 
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
 
-// Used by Heroku to listen to the correct port
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// Used by Heroku to listen to the correct port/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
