09b989efe8f8841dc6e5865b709305d1d06b07b3
Hughes <alex@alexjackhughes.com>
2018-01-12 13:48:58 +0000

    misspelling of production

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'prodiuction') {
+if(process.env.NODE_ENV === 'production') {
   // we are in production return prod keys
   module.exports = require('prod.js');
 } else {
